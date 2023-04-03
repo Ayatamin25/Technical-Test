0af2a3773309202105d5ff74fba0791607c37133
Ayat Amin <ayat.jafar.amin@gmail.com>
2023-04-03 13:42:47 -0400

    form submit complete

--- myjs.js
+++ myjs.js
@@ -4,7 +4,9 @@ const form = document.querySelector('form');
   const checkboxInput = document.getElementById('accurate');
   const errorElement = document.getElementById('error');
   const confirmation = document.getElementById('confirmation');
+  const img = confirmation.querySelector('img');
   const confirmationMessage = document.getElementById('confirmationMessage');
+  const smallez = document.getElementById('smallez');
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -27,15 +29,30 @@ const form = document.querySelector('form');
     const firstName = document.getElementById('name').value.split(' ')[0];
     const email = document.getElementById('email').value;
     const emailDomain = email.split('@')[1].split('.')[0];
-    confirmationMessage.textContent = `Welcome, ${firstName} a confirmation email has been sent to your ${emailDomain} account.`;
+    confirmationMessage.textContent = `Welcome, ${firstName}`;
+    smallez.textContent = ` Thanks for filling out the form. A confirmation email has been sent to your ${emailDomain} account.`;
+
     if (messages.length > 0) {
       errorElement.innerText = messages.join(', ');
     }else {
-      form.classList.add('hidden');
       confirmation.classList.remove('hidden');
+
+    const close = document.getElementsByClassName("close")[0];
+    confirmation.style.display = "block";
+    img.src = "welcome.jpg";
+
+     window.onclick = function(event) {
+      if (event.target == confirmation || event.target == close) {
+        confirmation.style.display = "none";
+        form.reset();
+
+      }
+    }
     }
   });
 
   function isValidEmail(email) {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+
   }
